1cb6e305b29ae345dcea6b036949ee4fdc192eb8
isonupandit11 <sonu.sp1123@gmail.com>
2022-05-24 15:34:39 +0530

    initial

--- pages/index.js
+++ pages/index.js
@@ -9,55 +9,60 @@ const Experience = dynamic(() => import("../containers/Experience"));
 const Projects = dynamic(() => import("../containers/Projects"));
 const Feedbacks = dynamic(() => import("../containers/Feedbacks"));
 const GithubProfileCard = dynamic(() =>
-	import("../components/GithubProfileCard")
+  import("../components/GithubProfileCard")
 );
 import { openSource } from "../portfolio";
 import SEO from "../components/SEO";
 
 export default function Home({ githubProfileData }) {
-	return (
-		<div>
-			<SEO
-				data={{
-					title: "Hanzla Tauqeer",
-					description:
-						"A passionate Full Stack Web Developer and Blockchain Developer.",
-					image: "https://avatars3.githubusercontent.com/u/59178380?v=4",
-					url: "https://developer-portfolio-1hanzla100.vercel.app",
-					keywords: [
-						"Hanzla",
-						"Hanzla Tauqeer",
-						"@1hanzla100",
-						"1hanzla100",
-						"Portfolio",
-						"Hanzla Portfolio ",
-						"Hanzla Tauqeer Portfolio",
-					],
-				}}
-			/>
-			<Navigation />
-			<Greetings />
-			<Skills />
-			<Proficiency />
-			<Education />
-			<Experience />
-			<Feedbacks />
-			<Projects />
-			<GithubProfileCard prof={githubProfileData} />
-		</div>
-	);
+  return (
+    <div>
+      <SEO
+        data={{
+          title: "isonupandit11",
+          description:
+            "A passionate Full Stack Web Developer and Blockchain Developer.",
+          image:
+            "https://lh3.googleusercontent.com/QqkJ8xWn8s68kV6LUTvlkjNgtR96p-6bHiHYN51v0VhsmEfqtYzGmMXtxaxBW_KvC0uQYiQ_i7a0cT2qG9XNH0cuirEHe9Ctb8uuu3UDoMkEQLjpiN7GE8bwK9nHUHmS-fB8NE8PrMI=w2400",
+          url: "https://isonupandit11.github.io/",
+          keywords: [
+            "isonupandit11",
+            "Sonu Sharma",
+            "@isonupandit11",
+            "isonupandit11",
+            "isonupandit11 followers",
+            "isonupandit11 instagram",
+            "isonupandit11 facebook",
+            "isonupandit11 developer",
+            "Portfolio",
+            "isonupandit11 Portfolio ",
+            "Sonu Sharma Portfolio",
+          ],
+        }}
+      />{" "}
+      <Navigation />
+      <Greetings />
+      <Skills />
+      <Proficiency />
+      <Education />
+      <Experience />
+      <Feedbacks />
+      <Projects />
+      <GithubProfileCard prof={githubProfileData} />{" "}
+    </div>
+  );
 }
 
 Home.prototype = {
-	githubProfileData: PropTypes.object.isRequired,
+  githubProfileData: PropTypes.object.isRequired,
 };
 
 export async function getStaticProps(_) {
-	const githubProfileData = await fetch(
-		`https://api.github.com/users/${openSource.githubUserName}`
-	).then((res) => res.json());
+  const githubProfileData = await fetch(
+    `https://api.github.com/users/${openSource.githubUserName}`
+  ).then((res) => res.json());
 
-	return {
-		props: { githubProfileData },
-	};
+  return {
+    props: { githubProfileData },
+  };
 }
